e9bbad28ee96e62aa66b8d18be97db3afc7892c3
vinayak soni <vianyaksoni977001@gmail.com>
2024-12-24 19:48:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./index.css"
+const App = () => {
+
+  return (
+    <div>
+      <img src="./image/menu.jpeg" alt="image not found" />
+      <img src="./image/menu1.jpeg" alt="image not found" />
+      <img src="./image/menu2.jpeg" alt="image not found" />
+      <img src="./image/menu3.jpeg" alt="image not found" />
+    </div>
+  )
+}
+
+export default App;
